815551b57e4e753b59f743b510dcb84f6838e34e
Paulo Luan <pauloluan.inova@gmail.com>
2014-08-12 16:17:27 -0300

    added fields from states and cities

--- crawler.py
+++ crawler.py
@@ -9,10 +9,10 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import urllib2
 import csv
@@ -23,54 +23,76 @@ import sys
 
 from bs4 import BeautifulSoup
 
-base_url = "http://cnes.datasus.gov.br/"
+base_url = 'http://cnes.datasus.gov.br/'
 state_json = {}
 
 # Parsear todos os estados pegando os ids
 def get_base_url():
     # http://cnes.datasus.gov.br/Lista_Tot_Es_Estado.asp
-    states_url = base_url + "Lista_Tot_Es_Estado.asp"
+    states_url = base_url + 'Lista_Tot_Es_Estado.asp'
     return states_url
 
 
 def get_state_url(state_id, state_name):
     # http://cnes.datasus.gov.br/Lista_Tot_Es_Municipio.asp?Estado=35&NomeEstado=SAO%20PAULO
-    state_url = base_url + "Lista_Tot_Es_Municipio.asp"
-    param_state_id = "Estado=35"
-    param_state_name = "NomeEstado=SAO%20PAULO"
-    state_url += "?" + param_state_id + "&" + param_state_name
+    state_url = base_url + 'Lista_Tot_Es_Municipio.asp'
+    param_state_id = 'Estado=35'
+    param_state_name = 'NomeEstado=SAO%20PAULO'
+    state_url += '?' + param_state_id + '&' + param_state_name
 
 
 def get_city_url(state_id, state_name, city_code):
     # http://cnes.datasus.gov.br/Lista_Es_Municipio.asp?VEstado=35&VCodMunicipio=354990&NomeEstado=SAO%20PAULO
-    city_url = base_url + "Lista_Es_Municipio.asp"
-    param_v_state = "VEstado=" + state_id  # 35
-    param_v_city_code = "VCodMunicipio=" + city_code  # 354990
-    city_url += "?" + param_v_state + "&" + param_v_city_code + "&" + param_state_name
+    city_url = base_url + 'Lista_Es_Municipio.asp'
+    param_v_state = 'VEstado=' + state_id  # 35
+    param_v_city_code = 'VCodMunicipio=' + city_code  # 354990
+    city_url += '?' + param_v_state + '&' + param_v_city_code + '&' + param_state_name
 
 
 def get_entity_url(state_id, unity_code, city_code):
     # http://cnes.datasus.gov.br/Exibe_Ficha_Estabelecimento.asp?VCo_Unidade=3549906891136&VEstado=35&VCodMunicipio=354990
-    entity_url = "Exibe_Ficha_Estabelecimento.asp"
-    param_unity_code = "VCo_Unidade=" + unity_code  # 3549906891136
-    param_state = "VEstado=" + state_id
-    param_city = "VCodMunicipio=" + city_code  # 354990
-    entity_url += "?" + param_unity_code + "&" + param_state + "&" + param_city
+    entity_url = 'Exibe_Ficha_Estabelecimento.asp'
+    param_unity_code = 'VCo_Unidade=' + unity_code  # 3549906891136
+    param_state = 'VEstado=' + state_id
+    param_city = 'VCodMunicipio=' + city_code  # 354990
+    entity_url += '?' + param_unity_code + '&' + param_state + '&' + param_city
 
 
 def get_plain_html(url):
-    print("Making a request: ", url)
+    # print('Making a request: ', url)
     response = urllib2.urlopen(url, timeout=6000)
     html = response.read()
     return html
+
+
+def get_offline_html(path):
+    from bs4 import BeautifulSoup
+
+    html = open(path)
+    plain_html = html.read()
+    tree = BeautifulSoup(plain_html)
+    return tree
 
 
 def get_states():
     states = []
     html = get_plain_html(get_base_url())
     tree = BeautifulSoup(html)
-    for state in tree('area'):
-        states.append(base_url + state['href'])  #TODO: append the entire url
+
+    div = tree.find('div',
+                    {'style': 'width:300; height:209; POSITION: absolute; TOP: 185px; LEFT: 400px; overflow:auto'})
+    table = div.find('table')
+
+    for row in table.find_all('tr'):
+        cols = row.find_all('td')
+
+        state_json = {}
+        state_json['estado_nome'] = cols[0].text
+        state_json['estado_total'] = cols[1].text
+        state_json['estado_%'] = cols[2].text
+        state_json['url'] = base_url + cols[0].find('a')['href']
+
+        states.append(state_json)  # TODO: append the entire url
     return states
 
 
@@ -78,10 +100,20 @@ def get_cities(state_url):
     cities = []
     html = get_plain_html(state_url)
     tree = BeautifulSoup(html)
-    table = tree.find('table')  # there is only one table on the state's page
-    links = table.findAll('a')
-    for link in links:
-        cities.append(base_url + link['href'])  #TODO: append the entire url
+
+    div = tree.find('div',
+                    {'style': 'width:450; height:300; POSITION: absolute; TOP: 201px; LEFT: 180px; overflow:auto'})
+    table = div.find('table')
+
+    for row in table.find_all('tr'):
+        cols = row.find_all('td')
+        city_json = {}
+        city_json['cidade_IBGE'] = cols[0].text
+        city_json['cidade_nome'] = cols[1].text
+        city_json['cidade_cadastrados'] = cols[2].text
+        city_json['url'] = base_url + cols[1].find('a')['href']
+        cities.append(city_json)  # TODO: append the entire url
+
     return cities
 
 
@@ -92,7 +124,7 @@ def get_entities(city_url):
     table = tree.find('table')  # there is only one table on the state's page
     links = table.findAll('a')
     for link in links:
-        entities.append(base_url + link['href'])  #TODO: append the entire url
+        entities.append(base_url + link['href'])  # TODO: append the entire url
     return entities
 
 
@@ -116,7 +148,7 @@ def parse_entity_data(entity_url):
 def list_to_json(rows):
     json = {}
     j = 0
-    #TODO: e se for par? vai ficar dado faltando?
+    # TODO: e se for par? vai ficar dado faltando?
     for i in range(len(rows)):
         try:
             temp_dict = dict(zip(rows[j], rows[j + 1]))
@@ -127,14 +159,18 @@ def list_to_json(rows):
     return json
 
 
-#TODO: mudar
+# TODO: mudar
 def save_csv(json):
     csvfile = open('output.csv', 'a')
     writer = csv.writer(csvfile, delimiter=',')
 
     if (file_is_empty('output.csv')):
+        # #print("\n\n")
+        ##print("Keys: ", json.keys())
         writer.writerow(json.keys())
 
+    # #print("\n\n")
+    ##print("values: ", json.values())
     writer.writerow(json.values())
 
 
@@ -143,25 +179,34 @@ def file_is_empty(path):
 
 
 def increase_progress_bar(progress):
-    sys.stdout.write("\r%d%%" % progress)
+    sys.stdout.write('\r%d%%' % progress)
     sys.stdout.flush()
+
+
+def merge_json(json1, json2):
+    merged_json = {key: value for (key, value) in (json1.items() + json2.items())}
+    if ('url' in merged_json): del merged_json['url']
+    return merged_json
 
 
 def initialize():
     states = get_states()
-    print("get states: ", len(states))
+    # print('States: ', len(states))
 
     for state in states:
-        cities = get_cities(state)
-        print("get cities: ", len(cities))
+        cities = get_cities(state['url'])
+        #print('Cities: ', len(cities))
 
         for city in cities:
-            entities = get_entities(city)
-            print("get entities: ", len(entities))
+            entities = get_entities(city['url'])
+            #print('Entities: ', len(entities))
 
             for entity in entities:
-                json = parse_entity_data(entity)
-                print(json)
+                base_json = merge_json(state, city)
+
+                entity_json = parse_entity_data(entity)
+                json = merge_json(base_json, entity_json)
+
                 save_csv(json)
 
 
